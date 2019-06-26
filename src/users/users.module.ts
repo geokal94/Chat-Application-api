929faf0b767330fe3088e2d08c90251de5de8900
George Kallis <giorgos.kallis.gr@gmail.com>
2019-06-26 13:25:06 +0300

    fixed users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersSchema } from './schemas/users.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Messages', schema: UsersSchema }]),
+  ],
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
 })
 export class UsersModule {}
